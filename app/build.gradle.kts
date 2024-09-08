a08d07f82697d64f381331caa178d84c8a22f15c
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-09-08 13:13:48 +0300

    update Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ android {
         compose = true
     }
     composeCompiler {
-        enableStrongSkippingMode = true
         reportsDestination = layout.buildDirectory.dir("compose_compiler")
     }
     packaging {
